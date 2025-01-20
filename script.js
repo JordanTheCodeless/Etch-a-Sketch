4a4a392edce41551cfdbec2eca2a1c3326826c45
JordanTheCodeless <131815445+JordanTheCodeless@users.noreply.github.com>
2025-01-20 18:14:24 +0000

    Changed grid to flex / will now implement the user being able to choose the desired grid

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-const etchContainer = document.querySelector("#container");
+let etchContainer = document.querySelector("#container");
+let reset = document.querySelector("#resetBtn");
+let enter = document.querySelector("#enterBtn");
+let gridAmount = document.querySelector("#gridSizeInput").va;
+let rows = 16;
+let columns = 16;
+let gridSize = 700;
+let tempDiv;
 
-let tempDiv
+etchContainer.style.width = `${gridSize}px`;
+etchContainer.style.height = `${gridSize}px`;
 function createDivs(){
-for(let i = 0; i < 256; i++){
+for(let i = 0; i < (rows*columns); i++){
     tempDiv = document.createElement("div");
+    tempDiv.style.width = `${(gridSize/columns)-2}px`;
+    tempDiv.style.height = `${(gridSize/rows)-2}px`;
+    tempDiv.style.backgroundColor ="white";
     tempDiv.classList.add("grid");
     etchContainer.appendChild(tempDiv)
 
 }}
 createDivs();
-const tempDivs = document.querySelectorAll(".grid");
 
-
+let tempDivs = document.querySelectorAll(".grid");
 tempDivs.forEach((thisDiv) => {
     thisDiv.addEventListener("mouseover",() =>{
         thisDiv.style.backgroundColor = "black";
     })
-})
+});
+reset.addEventListener("click", () =>{
+    tempDivs.forEach((thisDiv) => {
+        thisDiv.style.backgroundColor = "white";
+    })
+});
+enter.addEventListener("click", () =>{
 
+})
