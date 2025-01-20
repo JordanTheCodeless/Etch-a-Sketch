4ab9f47f65609864e8d48eee42cbf57dabe1acdc
JordanTheCodeless <131815445+JordanTheCodeless@users.noreply.github.com>
2025-01-20 19:46:29 +0000

    Users option of changing grid-size fully functional with option of resetting aswell

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let etchContainer = document.querySelector("#container");
 let reset = document.querySelector("#resetBtn");
 let enter = document.querySelector("#enterBtn");
-let gridAmount = document.querySelector("#gridSizeInput").va;
+let gridAmount = document.getElementById("gridSizeInput");
 let rows = 16;
 let columns = 16;
 let gridSize = 700;
@@ -9,7 +9,10 @@ let tempDiv;
 
 etchContainer.style.width = `${gridSize}px`;
 etchContainer.style.height = `${gridSize}px`;
-function createDivs(){
+function createDivs(columns,rows){
+
+etchContainer.innerHTML ="";
+
 for(let i = 0; i < (rows*columns); i++){
     tempDiv = document.createElement("div");
     tempDiv.style.width = `${(gridSize/columns)-2}px`;
@@ -18,20 +21,32 @@ for(let i = 0; i < (rows*columns); i++){
     tempDiv.classList.add("grid");
     etchContainer.appendChild(tempDiv)
 
-}}
-createDivs();
+}
+mouseOverDivs()
 
-let tempDivs = document.querySelectorAll(".grid");
-tempDivs.forEach((thisDiv) => {
+}
+createDivs(columns,rows);
+// need to create a function instead of just event listener due to newly created tempDivs disregarding event listener.
+function mouseOverDivs(){
+    tempDivs = document.querySelectorAll(".grid");
+    tempDivs.forEach((thisDiv) => {
     thisDiv.addEventListener("mouseover",() =>{
         thisDiv.style.backgroundColor = "black";
     })
 });
+}
+
+
 reset.addEventListener("click", () =>{
     tempDivs.forEach((thisDiv) => {
         thisDiv.style.backgroundColor = "white";
     })
 });
-enter.addEventListener("click", () =>{
 
+// event listener for the number input field;
+gridAmount.addEventListener('input', () => {
+    size = parseInt(gridAmount.value);
+    columns = size;
+    rows = size;
+    createDivs(columns,rows);
 })
